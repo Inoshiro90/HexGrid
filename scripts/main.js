82638c146bab5078f12953ef9cd1642a594287fd
Inoshiro90 <110790047+Inoshiro90@users.noreply.github.com>
2025-05-24 14:22:28 +0200

    Removed png rotation, svg and svg rotation download option temporary

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+canvas.width = canvasSize;
+canvas.height = canvasSize;
 
 // a LCG to use as a seedable RNG
 function lcg(seed) {
@@ -20,20 +22,6 @@ var canvasSize = 200;
 // 	context.translate(canvasSize * 0.5, canvasSize * 0.5);
 // 	context.scale(canvasSize * 0.45, canvasSize * 0.45);
 // }
-
-function updateCanvasSize() {
-	const canvasSize = parseInt(document.getElementById('input-field-size').value, 10) || 500;
-
-	const canvas = document.getElementById('canvas');
-	const context = canvas.getContext('2d');
-
-	canvas.width = canvas.height = canvasSize;
-
-	context.setTransform(1, 0, 0, 1, 0, 0); // Reset transform
-	context.translate(canvasSize * 0.5, canvasSize * 0.5);
-	context.scale(canvasSize * 0.45, canvasSize * 0.45);
-}
-updateCanvasSize();
 
 window.addEventListener('resize', updateCanvasSize);
 
@@ -92,6 +80,23 @@ function drawGridToSVG(grid, options) {
 
 	return svg;
 }
+
+function updateCanvasSize() {
+	const canvasSize = parseInt(document.getElementById('input-field-size').value, 10) || 500;
+
+	const canvas = document.getElementById('canvas');
+	const context = canvas.getContext('2d');
+	canvas.height = canvasSize;
+	canvas.width = canvas.height * 0.87;
+
+	// canvas.height = parseInt(document.getElementById('input-field-size').value, 10);
+	// canvas.width = Math.round((canvas.height * 157) / 180);
+
+	context.setTransform(1, 0, 0, 1, 0, 0); // Reset transform
+	context.translate(canvasSize * 0.435, canvasSize * 0.5);
+	context.scale(canvasSize * 0.5, canvasSize * 0.5);
+}
+updateCanvasSize();
 
 let dirty = true; // Initialzustand, falls nötig
 
@@ -166,7 +171,7 @@ document.getElementById('btn-download-png').addEventListener('click', () => {
 	const canvasSize = document.getElementById('input-field-size').value;
 
 	// Dateiname zusammensetzen
-	const filename = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize}.png`;
+	const filename = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize*0.87}.png`;
 
 	// Canvas als PNG exportieren
 	const image = canvas.toDataURL('image/png');
@@ -178,51 +183,51 @@ document.getElementById('btn-download-png').addEventListener('click', () => {
 	link.click();
 });
 
-document.getElementById('btn-download-png-rotation').addEventListener('click', () => {
-	const originalCanvas = document.getElementById('canvas');
-	if (!originalCanvas) {
-		alert('Canvas not found!');
-		return;
-	}
+// document.getElementById('btn-download-png-rotation').addEventListener('click', () => {
+// 	const originalCanvas = document.getElementById('canvas');
+// 	if (!originalCanvas) {
+// 		alert('Canvas not found!');
+// 		return;
+// 	}
 
-	const sides = document.getElementById('input-field-sideCount').value;
-	const seed = document.getElementById('input-field-seed').value;
-	const grouping = document.getElementById('input-field-grouping').value;
-	const relax = document.getElementById('dropdown-innerRelaxation').value;
-	const lineWidth = document.getElementById('input-field-line-width').value;
-	const lineColor = document.getElementById('input-field-line-color').value.replace('#', '');
-	const canvasSize = parseInt(document.getElementById('input-field-size').value, 10);
+// 	const sides = document.getElementById('input-field-sideCount').value;
+// 	const seed = document.getElementById('input-field-seed').value;
+// 	const grouping = document.getElementById('input-field-grouping').value;
+// 	const relax = document.getElementById('dropdown-innerRelaxation').value;
+// 	const lineWidth = document.getElementById('input-field-line-width').value;
+// 	const lineColor = document.getElementById('input-field-line-color').value.replace('#', '');
+// 	// const canvasSize = parseInt(document.getElementById('input-field-size').value, 10);
 
-	const offCanvas = document.createElement('canvas');
-	const ctx = offCanvas.getContext('2d');
-	offCanvas.width = offCanvas.height = canvasSize;
+// 	const offCanvas = document.createElement('canvas');
+// 	const ctx = offCanvas.getContext('2d');
+// 	offCanvas.width = offCanvas.height = canvasSize;
 
-	const angles = [0, 60, 120, 180, 240, 300];
-	const zip = new JSZip();
+// 	const angles = [0, 60, 120, 180, 240, 300];
+// 	const zip = new JSZip();
 
-	for (let angleDeg of angles) {
-		const angleRad = (angleDeg * Math.PI) / 180;
+// 	for (let angleDeg of angles) {
+// 		const angleRad = (angleDeg * Math.PI) / 180;
 
-		ctx.setTransform(1, 0, 0, 1, 0, 0);
-		ctx.clearRect(0, 0, canvasSize, canvasSize);
-		ctx.translate(canvasSize / 2, canvasSize / 2);
-		ctx.rotate(angleRad);
-		ctx.translate(-canvasSize / 2, -canvasSize / 2);
-		ctx.drawImage(originalCanvas, 0, 0, canvasSize, canvasSize);
+// 		ctx.setTransform(1, 0, 0, 1, 0, 0);
+// 		ctx.clearRect(0, 0, canvasSize, canvasSize);
+// 		ctx.translate(canvasSize / 2, canvasSize / 2);
+// 		ctx.rotate(angleRad);
+// 		ctx.translate(-canvasSize / 2, -canvasSize / 2);
+// 		ctx.drawImage(originalCanvas, 0, 0, canvasSize, canvasSize);
 
-		const dataUrl = offCanvas.toDataURL('image/png');
-		const base64Data = dataUrl.split(',')[1];
-		const filename = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize}_${angleDeg}.png`;
-		zip.file(filename, base64Data, {base64: true});
-	}
+// 		const dataUrl = offCanvas.toDataURL('image/png');
+// 		const base64Data = dataUrl.split(',')[1];
+// 		const filename = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize}_${angleDeg}.png`;
+// 		zip.file(filename, base64Data, {base64: true});
+// 	}
 
-	zip.generateAsync({type: 'blob'}).then((blob) => {
-		saveAs(
-			blob,
-			`hexgrid_rotations_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize}.zip`
-		);
-	});
-});
+// 	zip.generateAsync({type: 'blob'}).then((blob) => {
+// 		saveAs(
+// 			blob,
+// 			`hexgrid_rotations_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize}.zip`
+// 		);
+// 	});
+// });
 
 document.getElementById('btn-random-seed').addEventListener('click', () => {
 	const randomSeed = Math.floor(Math.random() * 65535) + 1;
@@ -239,85 +244,85 @@ document.getElementById('btn-random-seed').addEventListener('click', () => {
 	// }
 });
 
-document.getElementById('btn-download-svg').addEventListener('click', () => {
-	const svgElement = drawGridToSVG(grid, options);
-	const serializer = new XMLSerializer();
-	const svgString = serializer.serializeToString(svgElement);
-	const blob = new Blob([svgString], {type: 'image/svg+xml;charset=utf-8'});
+// document.getElementById('btn-download-svg').addEventListener('click', () => {
+// 	const svgElement = drawGridToSVG(grid, options);
+// 	const serializer = new XMLSerializer();
+// 	const svgString = serializer.serializeToString(svgElement);
+// 	const blob = new Blob([svgString], {type: 'image/svg+xml;charset=utf-8'});
 
-	const sides = document.getElementById('input-field-sideCount').value;
-	const seed = document.getElementById('input-field-seed').value;
-	const grouping = document.getElementById('input-field-grouping').value;
-	const relax = document.getElementById('dropdown-innerRelaxation').value;
-	const lineWidth = document.getElementById('input-field-line-width').value;
-	const lineColor = document.getElementById('input-field-line-color').value.replace('#', '');
-	const canvasSize = parseInt(document.getElementById('input-field-size').value, 10);
+// 	const sides = document.getElementById('input-field-sideCount').value;
+// 	const seed = document.getElementById('input-field-seed').value;
+// 	const grouping = document.getElementById('input-field-grouping').value;
+// 	const relax = document.getElementById('dropdown-innerRelaxation').value;
+// 	const lineWidth = document.getElementById('input-field-line-width').value;
+// 	const lineColor = document.getElementById('input-field-line-color').value.replace('#', '');
+// 	const canvasSize = parseInt(document.getElementById('input-field-size').value, 10);
 
-	const link = document.createElement('a');
-	link.href = URL.createObjectURL(blob);
-	link.download = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize}.svg`;
-	link.click();
-});
+// 	const link = document.createElement('a');
+// 	link.href = URL.createObjectURL(blob);
+// 	link.download = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor}_${canvasSize}x${canvasSize}.svg`;
+// 	link.click();
+// });
 
-document.getElementById('btn-download-svg-rotation').addEventListener('click', () => {
-	const sides = document.getElementById('input-field-sideCount').value;
-	const seed = document.getElementById('input-field-seed').value;
-	const grouping = document.getElementById('input-field-grouping').value;
-	const relax = document.getElementById('dropdown-innerRelaxation').value;
-	const lineWidth = document.getElementById('input-field-line-width').value;
-	const lineColor = document.getElementById('input-field-line-color').value;
-	const canvasSize = parseInt(document.getElementById('input-field-size').value, 10);
-	const scale = canvasSize * 0.45;
+// document.getElementById('btn-download-svg-rotation').addEventListener('click', () => {
+// 	const sides = document.getElementById('input-field-sideCount').value;
+// 	const seed = document.getElementById('input-field-seed').value;
+// 	const grouping = document.getElementById('input-field-grouping').value;
+// 	const relax = document.getElementById('dropdown-innerRelaxation').value;
+// 	const lineWidth = document.getElementById('input-field-line-width').value;
+// 	const lineColor = document.getElementById('input-field-line-color').value;
+// 	const canvasSize = parseInt(document.getElementById('input-field-size').value, 10);
+// 	const scale = canvasSize * 0.45;
 
-	const zip = new JSZip();
-	const angles = [0, 60, 120, 180, 240, 300];
+// 	const zip = new JSZip();
+// 	const angles = [0, 60, 120, 180, 240, 300];
 
-	angles.forEach((angleDeg) => {
-		const svgParts = [];
+// 	angles.forEach((angleDeg) => {
+// 		const svgParts = [];
 
-		svgParts.push(`<?xml version="1.0" standalone="no"?>`);
-		svgParts.push(
-			`<svg xmlns="http://www.w3.org/2000/svg" width="${canvasSize}" height="${canvasSize}" viewBox="-1 -1 2 2">`
-		);
-		svgParts.push(`<g transform="translate(0,0) scale(1) rotate(${angleDeg})">`);
+// 		svgParts.push(`<?xml version="1.0" standalone="no"?>`);
+// 		svgParts.push(
+// 			`<svg xmlns="http://www.w3.org/2000/svg" width="${canvasSize}" height="${canvasSize}" viewBox="-1 -1 2 2">`
+// 		);
+// 		svgParts.push(`<g transform="translate(0,0) scale(1) rotate(${angleDeg})">`);
 
-		svgParts.push(
-			`<g stroke="${lineColor}" stroke-width="${
-				lineWidth / 1000
-			}" fill="none" stroke-linecap="round" stroke-linejoin="round">`
-		);
+// 		svgParts.push(
+// 			`<g stroke="${lineColor}" stroke-width="${
+// 				lineWidth / 1000
+// 			}" fill="none" stroke-linecap="round" stroke-linejoin="round">`
+// 		);
 
-		for (let i = 0; i < grid.points.length; i++) {
-			const point = grid.points[i];
-			const neighbours = grid.neighbours[i];
+// 		for (let i = 0; i < grid.points.length; i++) {
+// 			const point = grid.points[i];
+// 			const neighbours = grid.neighbours[i];
 
-			for (let k = 0; k < neighbours.length; k++) {
-				const npoint = grid.points[neighbours[k]];
+// 			for (let k = 0; k < neighbours.length; k++) {
+// 				const npoint = grid.points[neighbours[k]];
 
-				svgParts.push(
-					`<line x1="${point[0]}" y1="${point[1]}" x2="${npoint[0]}" y2="${npoint[1]}" />`
-				);
-			}
-		}
+// 				svgParts.push(
+// 					`<line x1="${point[0]}" y1="${point[1]}" x2="${npoint[0]}" y2="${npoint[1]}" />`
+// 				);
+// 			}
+// 		}
 
-		svgParts.push(`</g></g></svg>`);
+// 		svgParts.push(`</g></g></svg>`);
 
-		const blob = new Blob([svgParts.join('\n')], {type: 'image/svg+xml'});
-		const filename = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor.replace(
-			'#',
-			''
-		)}_${canvasSize}x${canvasSize}_${angleDeg}.svg`;
+// 		const blob = new Blob([svgParts.join('\n')], {type: 'image/svg+xml'});
+// 		const filename = `hexgrid_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor.replace(
+// 			'#',
+// 			''
+// 		)}_${canvasSize}x${canvasSize}_${angleDeg}.svg`;
 
-		zip.file(filename, blob);
-	});
+// 		zip.file(filename, blob);
+// 	});
 
-	zip.generateAsync({type: 'blob'}).then((blob) => {
-		saveAs(
-			blob,
-			`hexgrid_rotations_svg_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor.replace(
-				'#',
-				''
-			)}_${canvasSize}x${canvasSize}.zip`
-		);
-	});
-});
+// 	zip.generateAsync({type: 'blob'}).then((blob) => {
+// 		saveAs(
+// 			blob,
+// 			`hexgrid_rotations_svg_${sides}_${seed}_${grouping}_${relax}_${lineWidth}_${lineColor.replace(
+// 				'#',
+// 				''
+// 			)}_${canvasSize}x${canvasSize}.zip`
+// 		);
+// 	});
+// });
